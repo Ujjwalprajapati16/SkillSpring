3b5968e8902170eb4e618b573d703a03de88cd19
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-04-27 15:59:59 +0530

    feat: update routing and structure for educator and student pages

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Educator = () => {
   return (
-    <div>Educator</div>
+    <div>
+      <h1>Educator</h1>
+      <p>Welcome to the Educator page!</p>
+      <p>This is where you can manage your courses and students.</p>
+      <p>More features coming soon!</p>
+      <div>
+        {<Outlet />}
+      </div>
+    </div>
   )
 }
 
