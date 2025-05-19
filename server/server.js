5c56d9489e92c1d3412ea541c3ba82946204b27d
neetesh <neeteshlodhi9555@email.com>
2025-05-20 00:13:55 +0530

    Backend setup $ Connection with database

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
+import connectDB from "./configs/mongodb.js";
+
+//initialize express
+const app = express()
+
+//middleware
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+          res.send("Api working");
+})
+
+//connect to the database
+await connectDB()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+          console.log(`server is running on ${PORT}`)
+})
+
+
