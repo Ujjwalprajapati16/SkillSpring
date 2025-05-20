1597f03bf5c50490ddc5d67406417a609638a301
neetesh <neeteshlodhi9555@email.com>
2025-05-20 19:16:03 +0530

    implement webhooks

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
+import {clerkWebhooks} from '../controllers/webhooks.js'
 
 //initialize express
 const app = express()
@@ -13,6 +14,8 @@ app.use(cors())
 app.get("/", (req, res) => {
           res.send("Api working");
 })
+
+app.post("/clerk",express.json(),clerkWebhooks)
 
 //connect to the database
 await connectDB()
