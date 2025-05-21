71bdd83efb155da5c8b13e903a8cacd9f4307e6f
neetesh <neeteshlodhi9555@email.com>
2025-05-21 16:47:04 +0530

    corrected user creation

--- server/server.js
+++ server/server.js
@@ -1,29 +1,31 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config'
+import { clerkWebHooks } from "./controllers/webhooks.js";
 import connectDB from "./configs/mongodb.js";
-import {clerkWebhooks} from '../controllers/webhooks.js'
+
 
 //initialize express
 const app = express()
 
 //middleware
 app.use(cors())
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
           res.send("Api working");
 })
 
-app.post("/clerk",express.json(),clerkWebhooks)
+app.post("/clerk",clerkWebHooks)
 
 //connect to the database
-await connectDB()
+const startServer = async () => {
+          await connectDB();
 
-const PORT = process.env.PORT || 5000
+          const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-          console.log(`server is running on ${PORT}`)
-})
-
-
+          app.listen(PORT, () => {
+                    console.log(`server is running on ${PORT}`)
+          })
+}
+startServer()
