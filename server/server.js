2f273ad5f8a3eed0c34273eeddc80dddf1c02310
neetesh <neeteshlodhi9555@email.com>
2025-05-21 23:04:20 +0530

    educator controller created

--- server/server.js
+++ server/server.js
@@ -1,30 +1,30 @@
 import express from "express";
 import cors from "cors";
-<<<<<<< HEAD
-import 'dotenv/config'
-import { clerkWebHooks } from "./controllers/webhooks.js";
-import connectDB from "./configs/mongodb.js";
 
-=======
-import "dotenv/config";
+import 'dotenv/config'
+import clerkWebHooks from "./controllers/webhooks.js";
 import connectDB from "./configs/mongodb.js";
-import clerkWebhooks from "./controllers/webhooks.js";
-import homeRoute from "./routes/HomeRoute.js";
->>>>>>> e08fc343073813500ec08a62d326ced89fc964a1
+import educatorRouter from './routes/educatorRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
+
+
+
 
 //initialize express
 const app = express();
 
 //middleware
-<<<<<<< HEAD
 app.use(cors())
 app.use(express.json());
-
+app.use(clerkMiddleware())
+//Routes
 app.get("/", (req, res) => {
           res.send("Api working");
 })
 
-app.post("/clerk",clerkWebHooks)
+app.post("/clerk", clerkWebHooks)
+
+app.use("/api/educator",educatorRouter)
 
 //connect to the database
 const startServer = async () => {
@@ -37,19 +37,8 @@ const startServer = async () => {
           })
 }
 startServer()
-=======
-app.use(cors());
 
-app.use("/", homeRoute);
 
-app.post("/clerk", express.json(), clerkWebhooks);
 
-//connect to the database
-await connectDB();
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
->>>>>>> e08fc343073813500ec08a62d326ced89fc964a1
